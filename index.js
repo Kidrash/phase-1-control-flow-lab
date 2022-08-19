27f7368b41dd2bd616a2e5b4d38075f912477608
Kidrash <markkaranja11@gmail.com>
2022-08-19 18:31:00 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+// Write your code here!
+  let freeSample ;
+  let overCharge
+  let noRide;
+
+  function scuberGreetingForFeet(i){
+  if (i <= 400){
+    return ( freeSample = "This one is on me!")
+  } if(i >=2500){
+    return (noRide = "No can do.")
+  } if(i >=2000 ){
+    return (overCharge = "I will gladly take your thirty bucks.")
+  }
+  // console.log(( freeSample = "This one is on me!"))
+  // console.log(overCharge = "I will gladly take your thirty bucks.")
+  // console.log(noRide = "No can do.")  
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  let destination; 
+  destination = city === 'NYC' ? ("Ok, sounds good."):("No go.");
+  return destination;
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
-}
+  switch(tip){
+    case tip === "generous":
+      return "Thank you so much."
+    case tip === "not so generous":
+      return "Thank you."
+    case tip === "thanks for everything":
+      return "Bye."
+  } 
+} 
